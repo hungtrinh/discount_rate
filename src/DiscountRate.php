281a76153df241695012b9632005f6cf6d271a53
Hưng Trịnh <trinhduchung@gmail.com>
2019-11-04 00:59:44 +0700

    Production code, done fourth spec for invalid customer type

--- src/DiscountRate.php
+++ src/DiscountRate.php
@@ -4,4 +4,5 @@ function discountRateByMembershipType($membershipType) {
     if ('platinum' === $membershipType) return 0.15;
     if ('gold' === $membershipType) return 0.1;
     if ('silver' === $membershipType) return 0.05;
+    throw new UnexpectedValueException('invalid membership type');
 }
