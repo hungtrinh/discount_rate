1bece0c20423311b2d5532d62a013b68d7848b89
Hưng Trịnh <trinhduchung@gmail.com>
2019-11-04 13:58:25 +0700

    Production code, done fifth spec, with api back compatibility with old api

--- src/DiscountRate.php
+++ src/DiscountRate.php
@@ -1,6 +1,14 @@
 <?php
+function discountRateByMembershipType($membershipType, $currentDate = null) {
+    if ( null === $currentDate) {
+        $currentDate = DateTime::createFromFormat('Y-m-d', '2019-11-29');
+    }
+    $isNotFriday = 5 !== (int) $currentDate->format('N');
+    $isNotNovember = 11 !== (int) $currentDate->format('n');
+    $dayOfMonth = (int) $currentDate->format('j');
+    $isNotBlackFriday = $isNotFriday || $isNotNovember  || $dayOfMonth < 23 || $dayOfMonth > 29;
 
-function discountRateByMembershipType($membershipType) {
+    if ($isNotBlackFriday) return 0;
     if ('platinum' === $membershipType) return 0.15;
     if ('gold' === $membershipType) return 0.1;
     if ('silver' === $membershipType) return 0.05;
